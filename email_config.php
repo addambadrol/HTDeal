012f3ed2b1ac77a92976920ab107ae6917bd2504
addambadrol <119830914+addambadrol@users.noreply.github.com>
2026-01-22 23:00:06 +0800

    Update email_config.php

--- email_config.php
+++ email_config.php
@@ -1,7 +1,7 @@
 <?php
 // Email Configuration for Password Reset
 define('SMTP_HOST', 'aguzzz.com');
-define('SMTP_PORT', 465);
+define('SMTP_PORT', 587);
 define('SMTP_USERNAME', 'htdeal@aguzzz.com'); // Gmail kau
 define('SMTP_PASSWORD', ']ykRw.U))q^t)ejL'); // App password dari Step 3
 define('SMTP_FROM_EMAIL', 'htdeal@aguzzz.com');
