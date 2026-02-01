c4f5d71f49c835e196b3d9bb3d1d2e5f5f8bf7b7
addambadrol <119830914+addambadrol@users.noreply.github.com>
2026-02-01 15:58:26 +0800

    Update about.php

--- pelanggan/about.php
+++ pelanggan/about.php
@@ -18,7 +18,7 @@ require_once '../db_config.php';
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif !important;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   }
 
   body {
